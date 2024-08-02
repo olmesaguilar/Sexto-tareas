65dea1907df80c3bef820fdcb3e191473a375166
olmesaguilar <olmesaguilar@outlook.es>
2024-08-02 09:53:43 -0500

    cambio prueba

--- 02GETPOST/controllers/alumnos.controller.php
+++ 02GETPOST/controllers/alumnos.controller.php
@@ -35,9 +35,47 @@ switch(%_GET["op"]){
             break;
         case "eliminar":
             $idAlumno = $_POST["idAlumno"];
+            $datos = $alumnos->eliminar($idAlumno);
+            echo json_encode($datos);
+            break;
+        mmmm
 
 
 
 }
-
+switch($_GET["op"]){
+    case "todos":
+        $datos = $alumnos->todos();
+        while ($row = mysqli_fetch_assoc($datos)){
+            $todos[] = $row;
+        }
+        echo json_encode($todos);
+        break;
+    case "uno":
+        $idAlumno = $_POST["idAlumno"];
+        $datos = $alumnos->uno($idAlumno);
+        $res = mysqli_fetch_assoc($datos);
+        echo json_encode($res);
+        break;
+    case "insertar":
+        $nombre = $_POST["nombre"];
+        $apellido = $_POST["apellido"];
+        $edad = $_POST["edad"];
+        $datos = $alumnos->insertar($nombre, $apellido, $edad);
+        echo json_encode($datos);
+        break;
+    case "actualizar":
+        $idAlumno = $_POST["idAlumno"];
+        $Nombre = $_POST["Nombre"];
+        $Apellido = $_POST["Apellido"];
+        $Edad = $_POST["Edad"];
+        $datos = $alumnos->actualizar($idAlumno, $Nombre, $Apellido, $Edad);
+        echo json_encode($datos);
+        break;
+    case "eliminar":
+        $idAlumno = $_POST["idAlumno"];
+        $datos = $alumnos->eliminar($idAlumno);
+        echo json_encode($datos);
+        break;
+}
 ?>
